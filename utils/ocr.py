f890c31f24e96afeb619469ff6356b1e699a1478
Vidh singh Chauhan <vidha.vsc@gmail.com>
2025-06-28 20:06:01 +0530

    img enhanced, ocr text logged

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,10 +1,31 @@
-import pyautogui
+from PIL import ImageEnhance, ImageFilter
 import pytesseract
+import pyautogui
+import time
+from datetime import datetime
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-img = pyautogui.screenshot() # takes the screenshot and stores in img
+ocr_log = {}
 
-text = pytesseract.image_to_string(img) # image to text now
+def txt():
+    img = pyautogui.screenshot().convert("RGB") # takes the screenshot and stores in img and makes the colour rgb which tesseract expects
+    img = img.convert("L")
+    enhancer = ImageEnhance.Contrast(img)
+    img = enhancer.enhance(2.0)
+    img = img.filter(ImageFilter.SHARPEN)
+    img = img.resize((img.width * 2, img.height * 2))
+    text = pytesseract.image_to_string(img) # image to text now
+    return text
 
-print(text)
+i = 0
+while(i<3):
+    timestamp = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    text = txt()
+    d_timestamp_text = {timestamp: text}
+    ocr_log.update(d_timestamp_text)
+
+    print(f"[{timestamp}] Captured text: {text}...")  # Print preview of text
+
+    time.sleep(5)
+    i+=1
