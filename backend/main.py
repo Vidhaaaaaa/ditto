d08ce41a2bc42a3bd8fc0c23994f10c12c81fd12
Vidh singh Chauhan <vidha.vsc@gmail.com>
2025-06-28 20:05:16 +0530

    removed text storage at supabase

--- backend/main.py
+++ backend/main.py
@@ -10,20 +10,8 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
 
+app = FastAPI()
 
-def insert_text(text):
-    url = f"{SUPABASE_URL}/rest/v1/sshot_text"
-    headers = {
-        "apikey": SUPABASE_KEY,
-        "Authorization": f"Bearer {SUPABASE_KEY}",
-        "Content-Type": "application/json",
-        "Prefer": "return=representation"
-    }
-    payload = {
-        "text": text  # make sure your table has a column called 'text'
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-
-    print("Status:", response.status_code)
-    print("Response:", response.text)
+@app.get("/")
+def root():
+    return {"message": "yes this works"}
